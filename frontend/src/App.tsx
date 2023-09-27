9ed96c9edcee5b6f10be00306dafb8ef5cef785b
yarocruz <yarocruz@gmail.com>
2023-09-26 21:31:30 -0400

    add the markup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,8 @@
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [message, setMessage] = useState("")
-
-  useEffect(() => {
-    getMessage()
-  }, [message])
+  const [message, setMessage] = useState(null)
 
   async function getMessage() {
     const response = await fetch('http://localhost:3030');
@@ -17,6 +13,14 @@ function App() {
   return (
     <>
      <h1>Rust + React</h1>
+
+     {message ? (
+      <div>Received: {message}</div>
+     ) : (
+     <div>
+      No Message
+     </div>
+     )}
 
      <button onClick={() => getMessage()}>Get Message</button> 
     </>
