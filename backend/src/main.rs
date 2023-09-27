5570f3a0b8d6a701291a5f7373569db962a791d3
yarocruz <yarocruz@gmail.com>
2023-09-26 20:33:00 -0400

    setup bones of our REST API server

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{
+    routing::get,
+    Json, Router,
+};
+use std::net::SocketAddr;
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new()
+        .route("/", get(handler));
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3030));
+    println!("Server started, listen on {}", addr);
+
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .expect("Failed to start server");
+}
+
+#[derive(serde::Serialize)]
+struct Message {
+    message: String,
+}
+
+async fn handler() -> Json<Message> {
+    Json(Message { message: String::from("Hello World") })
 }
